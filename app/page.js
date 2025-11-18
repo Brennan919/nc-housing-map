4a8db4ff6060babce8d12eb555ca4093589afa58
Brennan919 <brien.brennan@gmail.com>
2025-11-18 13:58:45 -0500

    Fix 1 for metadata

--- app/page.js
+++ app/page.js
@@ -15,14 +15,17 @@ export default function Home() {
           An interactive, county-by-county look at the state’s projected housing supply gap in 2029
         </h2>
         <p className="page-blurb">
-          This map was inspired by HR&amp;A Advisors’ housing scarcity dashboard
-          for the Florida Apartments Association. It is the first
-          interactive, county-level map of North Carolina’s housing shortage,
-          It visualizes the state's housing supply gap in 2029, based on research
-          conducted by Bowen National Research for the NC Chamber. This map was created
-          by Brien Brennan.
-          © 2025 Brien Brennan
-        </p>
+  This map was inspired by HR&amp;A Advisors&rsquo; housing scarcity dashboard
+  for the Florida Apartments Association. It is the first interactive,
+  county-level map of North Carolina&rsquo;s housing shortage. It visualizes
+  the state&apos;s housing supply gap in 2029, based on research conducted
+  by Bowen National Research for the NC Chamber. This map was created by
+  Brien Brennan. For questions or comments, email{" "}
+  <a href="mailto:brien.brennan@gmail.com">brien.brennan@gmail.com</a>.
+  &nbsp;&copy; 2025 Brien Brennan.
+</p>
+
+
       </header>
 
       <section className="page-map-wrapper">
